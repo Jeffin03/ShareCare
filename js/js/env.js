cc7caa74c1bc6db644863bc08ec5de7f27609a15
mrmathew0131 <lejomathewreji@gmail.com>
2026-03-22 00:36:06 +0530

    update npm test

---
+++ js/js/env.js
@@ -0,0 +1,7 @@
+window.__env = {
+    FIREBASE_API_KEY:             "get this from Jeffin",
+    FIREBASE_AUTH_DOMAIN:         "sharecare-dev.firebaseapp.com",
+    FIREBASE_PROJECT_ID:          "sharecare-dev",
+    FIREBASE_MESSAGING_SENDER_ID: "get this from Jeffin",
+    FIREBASE_APP_ID:              "get this from Jeffin"
+  }
